7f605401c92ff5011e81d0bab1bf8732723cd8ae
svenwal <sven@svenwal.de>
2022-01-21 11:30:51 +0100

    Fix typo on serverless filename

--- serverless/apply-serverless-plugin.sh
+++ serverless/apply-serverless-plugin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -i -X POST http://localhost:8001/plugins -F name=pre-function -F config.rewrite[1]=@zone.lua
+curl -i -X POST http://localhost:8001/plugins -F name=pre-function -F config.rewrite[1]=@zone-serverless.lua
